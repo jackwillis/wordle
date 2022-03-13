d4d38ca9483953a4a138d754384df2ada090e577
Jack Willis <jack@attac.us>
2022-03-12 20:37:43 -0600

    More basic game logic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::io;
 use std::io::Write;
 
-use wordle::PlayableWord;
+enum GameOutcome {
+    Lost,
+    Won,
+}
+
+static NUM_ALLOWED_GUESSES: i32 = 6;
 
 fn main() -> io::Result<()> {
-    let todays_word = PlayableWord::from("DRINK");
-    let mut remaining_guesses = 6;
+    let secret_word = wordle::dictionary::random_word();
 
-    let stdin = io::stdin();
+    let mut game_outcome = GameOutcome::Lost;
+    let mut remaining_guesses = NUM_ALLOWED_GUESSES;
 
     println!("WORDLE!");
 
@@ -15,13 +20,17 @@ fn main() -> io::Result<()> {
         print!("{} ", remaining_guesses);
         io::stdout().flush()?;
 
-        let mut buffer = String::new();
-        stdin.read_line(&mut buffer)?;
+        let play = wordle::PlayableWord::try_from(read_line()?);
 
-        match PlayableWord::try_from(buffer.trim()) {
+        match play {
             Ok(predicted_word) => {
-                let guess = todays_word.guess(&predicted_word);
+                let guess = secret_word.guess(&predicted_word);
                 println!("  {}", guess);
+
+                if guess.is_correct() {
+                    game_outcome = GameOutcome::Won;
+                    break;
+                }
 
                 remaining_guesses -= 1;
             }
@@ -31,5 +40,16 @@ fn main() -> io::Result<()> {
         }
     }
 
+    match game_outcome {
+        GameOutcome::Lost => println!("You lost. :("),
+        GameOutcome::Won => println!("You're a winner, baby!"),
+    }
+
     Ok(())
+}
+
+fn read_line() -> io::Result<String> {
+    let mut input_buffer = String::new();
+    io::stdin().read_line(&mut input_buffer)?;
+    Ok(String::from(input_buffer.trim()))
 }
