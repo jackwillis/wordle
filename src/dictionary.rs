465e2be82097164f530be9234ccde210d6b359a5
Jack Willis <jack@attac.us>
2022-03-26 16:26:24 -0500

    Add tests for dictionary

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -1,7 +1,7 @@
-use crate::Word;
-
 use rand::seq::SliceRandom;
 use std::str::FromStr;
+
+use crate::Word;
 
 // Static dictionary. Every item is guaranteed to be a valid [LegalWord].
 const DICTIONARY: &[&str] = &[
@@ -13,4 +13,19 @@ const DICTIONARY: &[&str] = &[
 pub fn random_word() -> Word {
     let word = *DICTIONARY.choose(&mut rand::thread_rng()).unwrap();
     Word::from_str(word).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::DICTIONARY;
+    use crate::Word;
+
+    #[test]
+    fn test_every_word_in_dictionary_is_valid() {
+        for word in DICTIONARY {
+            assert!(Word::from_str(word).is_ok());
+        }
+    }
 }
