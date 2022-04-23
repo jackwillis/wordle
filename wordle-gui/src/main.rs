4433e37ec10e60bf6e44eb563b65344086d70c52
Jack Willis <jack@attac.us>
2022-04-23 16:55:07 -0500

    Use fonts

--- wordle-gui/src/main.rs
+++ wordle-gui/src/main.rs
@@ -2,7 +2,9 @@
 // See <https://github.com/emilk/eframe_template/commit/86fe7b7b87e3a3868ce2648a3f2a63b6a044133f>.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use iced::{text_input, window, Align, Color, Column, Element, Sandbox, Settings, Text, TextInput};
+use iced::{
+    text_input, window, Align, Color, Column, Element, Font, Sandbox, Settings, Text, TextInput,
+};
 
 pub fn main() -> iced::Result {
     let settings = Settings {
@@ -32,6 +34,17 @@ enum Message {
     TextInputSubmitted,
 }
 
+// Fonts
+const NANUM_GOTHIC_BOLD: Font = Font::External {
+    name: "NanumGothicCoding-Bold",
+    bytes: include_bytes!("../fonts/NanumGothicCoding/NanumGothicCoding-Bold.ttf"),
+};
+
+const NANUM_GOTHIC_REGULAR: Font = Font::External {
+    name: "NanumGothicCoding-Regular",
+    bytes: include_bytes!("../fonts/NanumGothicCoding/NanumGothicCoding-Regular.ttf"),
+};
+
 impl Sandbox for App {
     type Message = Message;
 
@@ -50,7 +63,13 @@ impl Sandbox for App {
         match message {
             Message::TextInputChanged(value) => {
                 self.text_input_is_valid_word = value.parse::<wordle::Word>().is_ok();
-                self.text_input_value = value;
+
+                let text_input_value = value
+                    .to_uppercase()
+                    .chars()
+                    .filter(|c| c.is_ascii_alphabetic())
+                    .collect::<String>();
+                self.text_input_value = text_input_value;
             }
             Message::TextInputSubmitted => match self.text_input_value.parse::<wordle::Word>() {
                 Ok(word) => {
@@ -67,35 +86,46 @@ impl Sandbox for App {
 
     fn view(&mut self) -> Element<Message> {
         // Layout
-        let mut column = Column::new().padding(20).align_items(Align::Center);
+        let mut column = Column::new()
+            .padding(20)
+            .align_items(Align::Center)
+            .spacing(10);
 
         // Title
-        let title = Text::new("Wordle").size(50);
+        let title_label = "Wordle";
+        let title = Text::new(title_label).size(50).font(NANUM_GOTHIC_BOLD);
         column = column.push(title);
 
         // Text input
+        let placeholder = "Enter your guess";
         let text_input: Element<Message> = TextInput::new(
             &mut self.text_input_state,
-            "Submit your guess...",
+            placeholder,
             &self.text_input_value,
             Message::TextInputChanged,
         )
         .on_submit(Message::TextInputSubmitted)
         .padding(10)
+        .font(NANUM_GOTHIC_REGULAR)
         .into();
         column = column.push(text_input);
 
         // Flash
-        let nbsp = String::from("\u{00A0}");
-        let orangered = Color::from_rgb8(255, 69, 0);
-        let flash_message = self.flash_message.to_owned().unwrap_or(nbsp);
-        let flash_text: Element<Message> =
-            Text::new(flash_message).size(20).color(orangered).into();
-        column = column.push(flash_text);
+        if let Some(flash_message) = &self.flash_message {
+            let orangered = Color::from_rgb8(255, 69, 0);
+            let flash_text: Element<Message> = Text::new(flash_message)
+                .size(20)
+                .color(orangered)
+                .font(NANUM_GOTHIC_REGULAR)
+                .into();
+            column = column.push(flash_text);
+        }
 
         // Guessed words
         for word in &self.words {
-            let word_label = Text::new(word.to_owned()).size(20);
+            let word_label = Text::new(word.to_owned())
+                .size(32)
+                .font(NANUM_GOTHIC_REGULAR);
             column = column.push(word_label);
         }
 
