1e959b555df107a012680d0f9ff5dab82f5b2fe8
Jack Willis <jack@attac.us>
2022-04-23 16:17:19 -0500

    Re factor view function

--- wordle-gui/src/main.rs
+++ wordle-gui/src/main.rs
@@ -21,8 +21,8 @@ pub fn main() -> iced::Result {
 struct App {
     text_input_value: String,
     text_input_is_valid_word: bool,
-    text_input: text_input::State,
-    flash: Option<String>,
+    text_input_state: text_input::State,
+    flash_message: Option<String>,
     words: Vec<wordle::Word>,
 }
 
@@ -37,7 +37,7 @@ impl Sandbox for App {
 
     fn new() -> Self {
         Self {
-            text_input: text_input::State::focused(), // focus text input when app just opened
+            text_input_state: text_input::State::focused(), // focus text input when app just opened
             ..Default::default()
         }
     }
@@ -56,51 +56,49 @@ impl Sandbox for App {
                 Ok(word) => {
                     self.words.push(word);
                     self.text_input_value.clear();
-                    self.flash = None;
+                    self.flash_message = None;
                 }
                 Err(err) => {
-                    self.flash = Some(err.to_string());
+                    self.flash_message = Some(err.to_string());
                 }
             },
         }
     }
 
     fn view(&mut self) -> Element<Message> {
+        // Layout
         let mut column = Column::new().padding(20).align_items(Align::Center);
 
+        // Title
         let title = Text::new("Wordle").size(50);
         column = column.push(title);
 
-        let text_input = TextInput::new(
-            &mut self.text_input,
+        // Text input
+        let text_input: Element<Message> = TextInput::new(
+            &mut self.text_input_state,
             "Submit your guess...",
             &self.text_input_value,
             Message::TextInputChanged,
         )
         .on_submit(Message::TextInputSubmitted)
-        .padding(10);
-
+        .padding(10)
+        .into();
         column = column.push(text_input);
 
         // Flash
-        column = column.push(Views::display_flash(self.flash.as_ref()));
+        let nbsp = String::from("\u{00A0}");
+        let orangered = Color::from_rgb8(255, 69, 0);
+        let flash_message = self.flash_message.to_owned().unwrap_or(nbsp);
+        let flash_text: Element<Message> =
+            Text::new(flash_message).size(20).color(orangered).into();
+        column = column.push(flash_text);
 
+        // Guessed words
         for word in &self.words {
             let word_label = Text::new(word.to_owned()).size(20);
             column = column.push(word_label);
         }
 
         column.into()
-    }
-}
-
-struct Views {}
-impl Views {
-    fn display_flash(flash: Option<&String>) -> Element<'static, Message> {
-        let nbsp = String::from("\u{00A0}");
-        let orangered = Color::from_rgb8(255, 69, 0);
-        let flash_msg = flash.to_owned().unwrap_or(&nbsp);
-        let flash_text = Text::new(flash_msg).size(20).color(orangered);
-        flash_text.into()
     }
 }
