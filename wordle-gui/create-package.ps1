bb5e7267ee084e1dfa601b56d3267f21cb29ef96
Jack Willis <jack@attac.us>
2022-05-09 13:34:25 -0500

    dsadf

---
+++ wordle-gui/create-package.ps1
@@ -0,0 +1,20 @@
+Set-PSDebug -Trace 1
+
+$tag = $args[0]
+if (!$tag) {
+  throw "Usage: $($MyInvocation.MyCommand.Name) [tag]"
+}
+
+New-Item -Force -ItemType directory -Path dist\
+Copy-Item -Path target\release\wordle-gui.exe -Destination dist\Wordle.exe
+
+.\wordle-gui\vendor\rcedit-1.1.1\rcedit-x64.exe dist\Wordle.exe `
+  --set-icon wordle-gui\images\icon.ico `
+  --set-file-version ${tag} `
+  --set-product-version ${tag}
+
+Set-PSDebug -Trace 0
+
+Compress-Archive -Force `
+  -LiteralPath LICENSE.html , dist\Wordle.exe `
+  -DestinationPath "dist\Wordle-${tag}.zip"
